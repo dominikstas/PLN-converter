8814d9d4b3d3341c5fa6d8a5a70d4d8f8d2569c6
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-08 13:31:43 +0200

    added input_currency function, created database

--- currency.py
+++ currency.py
@@ -1,22 +1,37 @@
 #make database of currency, id of currency and site with the real time price
+money = 0
+currency = str
+
 def main():
-    money = 0
-    input_value()
+    while money == 0:
+        input_value()
+    input_currency()
+    check_currency(currency)
 #take an input from user what
 
 
 #input how many
 def input_value():
-    while money == 0:
         try:
+            global money 
             money = float(input("How much? "))
+            return(money)
         except ValueError:
             print("You must enter the number.")
+    
   
 
 #input to what
+def input_currency():
+    try:
+        global currency
+        currency =  str(input("What's currency? "))
+        return(currency)
+    except ValueError:
+        print("Please enter valid name. ")
 
-
+def check_currency(currency):
+     currency
 
 
 main()

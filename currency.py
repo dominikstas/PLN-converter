d2c952c4b5e8df074457ad0db886c2e86cffcb3f
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-13 11:00:48 +0200

    added "check prices" function

--- currency.py
+++ currency.py
@@ -1,4 +1,8 @@
 import sqlite3
+
+db = sqlite3.connect("currency.db")
+cursor = db.cursor()
+
 #make database of currency, id of currency and site with the real time price
 money = 0
 currency = str
@@ -7,7 +11,7 @@ def main():
     while money == 0:
         input_value()
     input_currency()
-    check_currency(currency)
+    
 #take an input from user what
 
 
@@ -15,7 +19,10 @@ def main():
 def input_value():
         try:
             global money 
-            money = float(input("How much? (Deafult currency is PLN) "))
+            money = float(input("How much? (Deafult currency is PLN). If you want check current prices, enter '0' "))
+            if money == 0:
+                 show()
+                 input_value()
             return(money)
         except ValueError:
             print("You must enter the number.")
@@ -27,14 +34,24 @@ def input_currency():
     try:
         global currency
         currency =  str(input("What's currency? "))
-        return(currency)
+        check_currency(currency)
+        if check_currency == "valid":
+             return(currency)
     except ValueError:
         print("Please enter valid name. ")
 
 def check_currency(currency):
      currency
      
-
+def show():
+     cursor.execute('''
+                    select *
+                    from currency
+                    ''')
+     
+     rows = cursor.fetchall()
+     for r in rows:
+          print(r[1],r[2],r[3])
 
 main()
 
